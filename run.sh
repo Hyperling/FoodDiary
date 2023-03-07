0378fbf2d5c2885381a412ba5d1725b2d62d8fea
Hyperling <me@hyperling.com>
2023-03-07 05:15:22 -0600

    Run the Node program.

--- run.sh
+++ run.sh
@@ -3,10 +3,20 @@
 # Developer: Hyperling
 # Purpose: Install all dependencies and run the project.
 
+# Install any system packages.
 if [[ -z `which npm` ]]; then
+	echo "NPM needs to be installed."
 	sudo apt install npm
+else
+	echo "NPM is already installed."
 fi
 
+# Install/update any project modules.
+echo "Ensuring NPM packages are correct."
 npm install package.json
 
+# Start backend
+node ./server.js
+
+# Start frontend.
 # ???
